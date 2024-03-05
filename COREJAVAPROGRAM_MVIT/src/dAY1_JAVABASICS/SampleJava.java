27e05f923ad3c3a8e3ae76a891e352ee9443e2f4
Student <Student@DESKTOP-IK8IID0>
2024-03-05 15:46:24 +0530

    first java program

---
+++ COREJAVAPROGRAM_MVIT/src/dAY1_JAVABASICS/SampleJava.java
@@ -0,0 +1,19 @@
+package dAY1_JAVABASICS;
+import java.util.*;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		
+		System.out.println("enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+       System.out.println("hello world");
+       System.out.println("addition of a="+a+" and b="+b+" is :"+c);
+	}
+
+}
---
+++ COREJAVAPROGRAM_MVIT/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module COREJAVAPROGRAM_MVIT {
+}
